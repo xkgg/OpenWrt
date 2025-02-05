9eefa0e12aee3165be34c2e6a5508aa19b6139f6
昕风挽笑 <38105508+xkgg@users.noreply.github.com>
2025-02-05 09:29:00 +0800

    Update install_compilation_dependencies.sh

--- scripts/install_compilation_dependencies.sh
+++ scripts/install_compilation_dependencies.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sudo -E apt-mark hold grub-efi-amd64-signed
 sudo -E apt -y full-upgrade
-sudo apt install -y build-essential clang flex bison g++ gawk \
+sudo apt install -y build-essential libncursesw5-dev libncurses5-dev clang flex bison g++ gawk \
 gcc-multilib g++-multilib gettext git libncurses-dev libssl-dev \
-python3-distutils python3-setuptools rsync swig unzip zlib1g-dev file wget || sudo apt install -y build-essential clang flex bison g++ gawk \
+python3-distutils python3-setuptools python3 ccache git-core rsync swig unzip zlib1g-dev file wget || sudo apt install -y build-essential libncursesw5-dev libncurses5-dev clang flex bison g++ gawk \
 gcc-multilib g++-multilib gettext git libncurses-dev libssl-dev \
-python3-distutils python3-setuptools rsync swig unzip zlib1g-dev file wget
+python3-distutils python3-setuptools python3 ccache git-core rsync swig unzip zlib1g-dev file wget
 sudo -E systemctl daemon-reload 
 sudo -E apt -y autoremove --purge
 sudo -E apt clean
