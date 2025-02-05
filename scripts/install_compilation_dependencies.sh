219faf9d53ec66df81ccbf528b8c07addf67f516
昕风挽笑 <38105508+xkgg@users.noreply.github.com>
2025-02-05 16:29:00 +0800

    Update install_compilation_dependencies.sh

--- scripts/install_compilation_dependencies.sh
+++ scripts/install_compilation_dependencies.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 sudo -E apt-mark hold grub-efi-amd64-signed
 sudo -E apt -y full-upgrade
-sudo apt install -y build-essential libncursesw5-dev libncurses5-dev clang flex bison g++ gawk \
-gcc-multilib g++-multilib gettext git libncurses-dev libssl-dev \
-python3-distutils python3-setuptools python3 ccache git-core rsync swig unzip zlib1g-dev file wget || sudo apt install -y build-essential libncursesw5-dev libncurses5-dev clang flex bison g++ gawk \
-gcc-multilib g++-multilib gettext git libncurses-dev libssl-dev \
-python3-distutils python3-setuptools python3 ccache git-core rsync swig unzip zlib1g-dev file wget
+sudo apt install -y ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential \
+bzip2 ccache clang cmake cpio curl device-tree-compiler flex gawk gcc-multilib g++-multilib gettext \
+genisoimage git gperf haveged help2man intltool libc6-dev-i386 libelf-dev libfuse-dev libglib2.0-dev \
+libgmp3-dev libltdl-dev libmpc-dev libmpfr-dev libncurses5-dev libncursesw5-dev libpython3-dev \
+libreadline-dev libssl-dev libtool llvm lrzsz msmtp ninja-build p7zip p7zip-full patch pkgconf \
+python3 python3-pyelftools python3-setuptools qemu-utils rsync scons squashfs-tools subversion \
+swig texinfo uglifyjs upx-ucl unzip vim wget xmlto xxd zlib1g-dev || sudo apt install -y ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential \
+bzip2 ccache clang cmake cpio curl device-tree-compiler flex gawk gcc-multilib g++-multilib gettext \
+genisoimage git gperf haveged help2man intltool libc6-dev-i386 libelf-dev libfuse-dev libglib2.0-dev \
+libgmp3-dev libltdl-dev libmpc-dev libmpfr-dev libncurses5-dev libncursesw5-dev libpython3-dev \
+libreadline-dev libssl-dev libtool llvm lrzsz msmtp ninja-build p7zip p7zip-full patch pkgconf \
+python3 python3-pyelftools python3-setuptools qemu-utils rsync scons squashfs-tools subversion \
+swig texinfo uglifyjs upx-ucl unzip vim wget xmlto xxd zlib1g-dev
 sudo -E systemctl daemon-reload 
 sudo -E apt -y autoremove --purge
 sudo -E apt clean
