22741357476e68144dccfc1835c3dd100ee90663
昕风挽笑 <38105508+xkgg@users.noreply.github.com>
2025-02-06 23:49:46 +0800

    Update install_compilation_dependencies.sh

--- scripts/install_compilation_dependencies.sh
+++ scripts/install_compilation_dependencies.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo -E apt-mark hold grub-efi-amd64-signed
 sudo -E apt -y full-upgrade
+sudo apt - get update 
+sudo apt - get install b43 - fwcutter
 sudo apt install -y ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential \
 bzip2 ccache clang cmake cpio curl device-tree-compiler flex gawk gcc-multilib g++-multilib gettext \
 genisoimage git gperf haveged help2man intltool libc6-dev-i386 libelf-dev libfuse-dev libglib2.0-dev \
