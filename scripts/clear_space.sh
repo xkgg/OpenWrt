50f8f68ac4bf185cb0131786035cb8c88ddd58d8
昕风挽笑 <38105508+xkgg@users.noreply.github.com>
2025-04-03 12:31:28 +0800

    Update clear_space.sh

--- scripts/clear_space.sh
+++ scripts/clear_space.sh
@@ -3,4 +3,6 @@ sudo rm -rf /etc/apt/sources.list.d/* /usr/share/dotnet /usr/local/lib/android /
 sudo -E apt-mark hold grub-efi-amd64-signed
 sudo -E apt update
 sudo -E apt -y purge azure-cli* docker* ghc* zulu* llvm* firefox google* dotnet* powershell* openjdk* mysql* php* mongodb* dotnet* snap* moby* 2>&1
+sudo -E apt-get -qq autoremove --purge
+sudo -E apt-get -qq clean
 exit 0
