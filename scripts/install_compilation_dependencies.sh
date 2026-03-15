af7edc9b18945e91786ea106fb27758084d96ab8
昕风挽笑 <38105508+xkgg@users.noreply.github.com>
2026-03-15 11:54:31 +0800

    Update install_compilation_dependencies.sh

--- scripts/install_compilation_dependencies.sh
+++ scripts/install_compilation_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo -E apt-get -y update
-sudo -E apt-get -y install $(curl -fsSL is.gd/depends_ubuntu_2204)
+sudo -E apt-get -y install $(curl -fsSL https://raw.githubusercontent.com/0118Add/Openwrt-CI/refs/heads/main/depends-ubuntu-2204)
 sudo -E systemctl daemon-reload
 sudo -E apt-get -y autoremove --purge
 sudo -E apt clean
